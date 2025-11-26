e2905a029d5b70a82d6459af9771443af295cf58
Ahmad  Al Dibo <ahmadaldibo212009@gmail.com>
2025-11-26 21:22:32 +0100

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer: React.FC = () => {
-  return (
-    <footer className="bg-gray-900 text-gray-300 text-center py-6 mt-10">
-      <p>© {new Date().getFullYear()} BedrijfNaam. Alle rechten voorbehouden.</p>
-    </footer>
-  );
-};
-
-export default Footer;
